e302dd12c8daa79477c4d838bf26e1eb658376f6
kostasgo <k.gogas@hotmail.com>
2022-08-07 16:03:38 +0300

    save user

--- src/main/java/org/example/auctionhouse/service/UserService.java
+++ src/main/java/org/example/auctionhouse/service/UserService.java
@@ -1,4 +1,9 @@
 package org.example.auctionhouse.service;
 
+import org.example.auctionhouse.model.User;
+import org.example.auctionhouse.web.dto.UserRegistrationDto;
+
 public interface UserService {
+
+    User save(UserRegistrationDto registrationDto);
 }
