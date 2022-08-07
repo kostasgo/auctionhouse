09445e1e1a40d153debd0b8d60418ea5fc5a7ed1
kostasgo <k.gogas@hotmail.com>
2022-08-07 15:54:44 +0300

    User & Role Tables + join Table for many-to-many relationship

---
+++ src/main/java/org/example/auctionhouse/model/Role.java
@@ -0,0 +1,19 @@
+package org.example.auctionhouse.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+}
