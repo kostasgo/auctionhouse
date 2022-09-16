8c43784cda9a4cae3fdf0503fe931e0c6e726ee8
konstantinos Gkogkas <k.gogas@hotmail.com>
2022-09-16 13:44:47 +0300

    load an auction from start (also 2 repositories i didnt commit before)

---
+++ src/main/java/org/example/auctionhouse/repository/AuctionRepository.java
@@ -0,0 +1,9 @@
+package org.example.auctionhouse.repository;
+
+import org.example.auctionhouse.model.Auction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuctionRepository extends JpaRepository<Auction, Long> {
+}
