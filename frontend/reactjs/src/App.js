371ad3206a2b4a5991640f8e0d5bbba8b040355d
kostas antzou <kostas.best@gmail.com>
2022-09-17 15:07:26 +0300

    wow changes!

--- frontend/reactjs/src/App.js
+++ frontend/reactjs/src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
-import NavigationBar from './components/NavigationBar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import NavigationBar from './components/sharedComponents/NavigationBar';
+import AuctionsListPage from './components/pages/AuctionsListPage/AuctionsListPage';
 
 function App() {
   return (
     <div className="App">
       <NavigationBar/>
+      <Router>
+        <Routes>
+         
+          <Route path="/explore" element={<AuctionsListPage />} />
+
+        </Routes>
+      </Router>
     </div>
   );
 }
