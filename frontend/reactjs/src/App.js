ccff17d234d28ac65473bcb8a99bbeae586f7ae1
kostas antzou <kostas.best@gmail.com>
2022-09-20 19:45:18 +0300

    AuctionPage fixes, NewAuction template,

--- frontend/reactjs/src/App.js
+++ frontend/reactjs/src/App.js
@@ -5,6 +5,8 @@ import { Container } from 'react-bootstrap'
 
 import NavigationBar from './components/sharedComponents/NavigationBar';
 import AuctionsList from './components/Auction/AuctionsList';
+import AuctionPage from './components/Auction/AuctionPage';
+import NewAuction from './components/Auction/NewAuction';
 import Login from './components/User/Login';
 import Register from './components/User/Register';
 
@@ -19,6 +21,8 @@ function App() {
           <Routes>
             <Route path="/" element={<></>} />
             <Route path="/auctions" element={<AuctionsList />} />
+            <Route path="/auctionpage" element={<AuctionPage />} />
+            <Route path="/newauction" element={<NewAuction />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
 
---
+++ frontend/reactjs/src/components/Auction/NewAuction.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Routes, Route, Navigate, json } from 'react-router-dom';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap'
+import ListGroup from 'react-bootstrap/ListGroup';
+import Carousel from 'react-bootstrap/Carousel';
+import axios from 'axios'
+import "./NewAuction.css"
+import AuctionsList from './AuctionsList';
+
+function calcDifference(dt1, dt2) {
+    var diff = (dt1 - dt2) / 1000;
+    diff /= 60;
+    return Math.abs(Math.round(diff));
+}
+
+
+export default class NewAuction extends Component {
+    constructor(props, context) {
+        super(props);
+        console.log(this.props.data_tranfer);
+        this.state = {
+            toBack : false
+        };
+    }
+
+    componentDidMount() {
+        
+    }
+    
+    render() {
+
+        const handleBack = () => {
+            console.log("BACK CLICKED");
+            console.log(this.state.auction.seller);
+            this.setState({toBack:true});
+        };
+   
+
+        return (!this.state.toBack) ? 
+        <>
+            <span>HALLO</span>
+        </>
+        : 
+        <>
+            <AuctionsList/>
+        </>
+    }
+}
